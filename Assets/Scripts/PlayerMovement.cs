410411f1d16104ba6cea4accde219966775340e6
Maxi1324 <maxi1234567890fischer@gmail.com>
2023-04-29 15:52:45 +0200

    update scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,41 +10,73 @@ public class PlayerMovement : MonoBehaviour
     public float DeadZoneMouse = 0.1f;
     public float MouseSpeedX = 100;
 
+    public float jumpForce = -30;
+    public float grav = 3;
+
+    public Transform GroundPos;
+    public LayerMask mask;
+
+    private float v;
+    private bool stopV = false;
+
+    public CharacterController character;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        var onGround = Physics.CheckSphere(GroundPos.position, 0.5f, mask);
+
+       
+
         var speedMod = speed;
         var hori = Input.GetAxis("Horizontal");
         var verti = Input.GetAxis("Vertical");
 
         var mousex = Input.GetAxis("Mouse X");
 
-
+        var Jump = Input.GetButtonDown("Jump");
         var Sprint = Input.GetButton("LeftShift");
+
 
         if (Sprint)
         {
             speedMod *= SprintMult;
         }
+        if (onGround)
+        {
+            v = -0;
+        }
+        else
+        {
+            v += grav * Time.deltaTime ;
+        }
+        if (Jump)
+        {
+            v = jumpForce;
+        }
 
-        if(Mathf.Abs(hori) > deadZone)
+        
+        character.Move(Vector3.down * v *Time.deltaTime);
+
+        if (Mathf.Abs(hori) > deadZone)
         {
             float lSpeed = speedMod * 0.65f;
-            transform.position += transform.right * hori*lSpeed*Time.deltaTime;
+            character.Move(transform.right * hori * lSpeed * Time.deltaTime);
         }
         if (Mathf.Abs(verti) > deadZone)
         {
             float lSpeed = speedMod*((verti > 0) ? 1 : 0.5f);
-            transform.position += transform.forward * verti * lSpeed * Time.deltaTime;
+            character.Move(transform.forward * verti * lSpeed * Time.deltaTime);
         }
 
         if(Mathf.Abs(mousex) > DeadZoneMouse)
         {
             transform.Rotate(0, MouseSpeedX * mousex*Time.deltaTime,0);
         }
+
     }
 }
